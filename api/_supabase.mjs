9f95fe0294d268bb1cadbfd7716f4d2decd5ad4d
luiscordobad <luiscordobad@gmail.com>
2025-09-05 19:01:23 -0600

    Update _supabase.mjs

--- api/_supabase.mjs
+++ api/_supabase.mjs
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Usamos SERVICE_ROLE en server; si no existe, cae a la anon (solo para cliente)
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE || process.env.VITE_SUPABASE_ANON_KEY;
 
 export const supa = createClient(supabaseUrl, supabaseKey, {
   auth: { persistSession: false }
 });
+
 
