400eb4d4205b7803fc5052c39b7ac32f436e4ead
luiscordobad <luiscordobad@gmail.com>
2025-09-05 22:10:46 -0600

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,2 +1,8 @@
+// src/lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
-export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
